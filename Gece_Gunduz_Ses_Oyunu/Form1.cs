b6cedf87a8b6cdc7ed268f1b94212af419c5c7cf
Salih <f4t1hsalih@gmail.com>
2023-06-19 16:03:27 +0300

    Add project files.

---
+++ Gece_Gunduz_Ses_Oyunu/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Gece_Gunduz_Ses_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int tkr = rnd.Next(1, 3);
+            if (tkr == 1)
+            {
+                SoundPlayer ses = new SoundPlayer();
+                ses.SoundLocation = "Gece.wav";
+                ses.Play();
+            }
+            if (tkr == 2)
+            {
+                SoundPlayer ses = new SoundPlayer();
+                ses.SoundLocation = "Gunduz.wav";
+                ses.Play();
+            }
+        }
+    }
+}
---
+++ Gece_Gunduz_Ses_Oyunu/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Gece_Gunduz_Ses_Oyunu
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
